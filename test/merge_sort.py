f1e3a55c0245e9d7557d07707bc48f5db1b73bdd
mustilica <ilhan.mstf@gmail.com>
2017-08-12 16:47:05 +0300

    Comment update

--- test/merge_sort.py
+++ test/merge_sort.py
@@ -22,8 +22,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
+
 """
-Test to meausure the performance of merging and sorting trends 
+Test to meausure the performance of merging and sorting trends
 with two different code.
 """
 
